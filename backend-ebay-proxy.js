40106a3a759ccaadd585c095b674975addd45953
lelik827 <yoshalosh@gmail.com>
2025-08-07 10:55:05 -0400

    Update backend-ebay-proxy.js

--- backend-ebay-proxy.js
+++ backend-ebay-proxy.js
@@ -1,9 +1,7 @@
 import fetch from 'node-fetch';
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
 
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
